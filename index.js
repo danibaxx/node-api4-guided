030b75cd92c53e5e3da91a6f0df24e8c3d24b90b
Danielle B <danibaxx@hotmail.com>
2019-12-11 18:48:14 -0700

    added new enviroment during lecture

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express")
+// moved to package.json with the -r flag(require) with dotenv/config
+// this is better to use when running it locally but not during production
+// const dotenv = require('dotenv');
+
+// dotenv.config()
 
 const app = express()
-const host = "0.0.0.0"
+const host = "localhost"
 const port = 8080
 
 app.use((req, res, next) => {
@@ -12,6 +17,7 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
 	res.json({
 		message: "Welcome to our API",
+		cohort: process.env.LAMBDA_COHORT,
 	})
 })
 
