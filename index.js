d51c2298018b9fa7dfc158abb5263cb483ee8800
Danielle B <danibaxx@hotmail.com>
2019-12-11 19:05:02 -0700

    changes for heroku lecture

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const express = require("express")
 // dotenv.config()
 
 const app = express()
-const host = "localhost"
-const port = 8080
+const host = process.env.HOST || "0.0.0.0"
+const port = process.env.PORT || 8080
 
 app.use((req, res, next) => {
 	console.log(`[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`)
